e3161b60b78b5cfbeba4875acbbb2f138d5da051
Yifan Cai <ycai@apache.org>
2021-05-11 15:31:53 -0700

    Improve metadata exception handling
    Patch by Yifan Cai; reviewed by Marcus Eriksson, Dinesh Joshi for CASSANDRA-16211

--- common/src/main/java/org/apache/cassandra/diff/RetryStrategy.java
+++ common/src/main/java/org/apache/cassandra/diff/RetryStrategy.java
@@ -1,6 +1,26 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.apache.cassandra.diff;
 
 import java.util.concurrent.Callable;
+import java.util.function.Function;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -16,12 +36,28 @@ public abstract class RetryStrategy {
     protected abstract boolean shouldRetry();
 
     public final <T> T retry(Callable<T> retryable) throws Exception {
+        return retryIfNot(retryable);
+    }
+
+    /**
+     * Retry a retryable.
+     * Rethrow the exception from retryable if no more retry is permitted or the thrown exception is in the exclude list.
+     */
+    @SafeVarargs
+    public final <T> T retryIfNot(Callable<T> retryable, Class<? extends Exception>... excludedExceptions) throws Exception {
+        Function<Exception, Boolean> containsException = ex -> {
+            for (Class<? extends Exception> xClass : excludedExceptions) {
+                if (xClass.isInstance(ex))
+                    return true;
+            }
+            return false;
+        };
         while (true) {
             try {
                 return retryable.call();
             }
             catch (Exception exception) {
-                if (!shouldRetry()) {
+                if (containsException.apply(exception) || !shouldRetry()) {
                     throw exception;
                 }
                 logger.warn("Retry with " + toString());
